ebcbeef469b2ad91cf8f4a022ec987d868d7cff6
Faiza Siddiqui <faizasiddiqui@uregina.ca>
2021-11-23 15:31:45 +0300

    first commit

---
+++ MorseCode.cpp
@@ -0,0 +1,55 @@
+/*****************************************************************************************************************************
+	* @file MorseCode.cpp
+    * @brief	Module Name is Morse Code Encoder Decoder
+    * @author	Faiza Fatma Siddiqui, Student ID: 200473896
+    * @date		25-11-2021 (Created & Modified)
+    * @details 	This program prompts the user to determine the size of array. 
+	* 	- It restricts the user from entering any invalid entries for
+	* 	size of array and allows to enter only positive whole numbers.
+	* 	- It generates a random array of the size given by the user.
+	* 	- It sorts the array in Ascending order using Shaker Sort Algorithm.
+	* 	- It sorts the array in Descending order using Shaker Sort Algorithm.
+	* 	- It prints the time taken to perform both sortings.
+    * Purpose	CS 700 - Software Development Fundamentals - Assignment 4 
+	* Method Output: An array is sorted in ascending and descending order & printed. The time taken in both sortings is calculated & printed.
+    * @bug		No known bugs.
+	* @warning	Improper use can crash the program while taking input from the user
+*****************************************************************************************************************************/
+
+/** This header file includes all standard libraries */
+#include <bits/stdc++.h>
+
+/// chrono library deals with time, by means of clocks, time points, durations
+#include <chrono>
+
+///header file that contains swap function
+#include "swap.h"
+
+/// This file includes all standard libraries
+using namespace std;
+
+
+/*****************************************************************************************************************************
+ 	* @brief	Module Name is Main Function
+    * @author	Faiza Fatma Siddiqui
+    * @date	25-11-2021 (Created/Modified)
+    * @details Purpose is to print message to user, to print the sorted array in a Ascending order & Descending order using Shaker Sort Algorithm
+    * Description: This function prompts the user to determine the size of array. 
+	*	- It restricts the user from entering any invalid entries for size of array and allows to enter only positive whole numbers.
+	*	- It generates a random array of the size given by the user and prints it
+	*	- It calls the function for sorting array in Ascending order & displays the sorted array
+	*	- It calls the function for sorting array in Descending order & displays the sorted array
+	*	-
+    * @param	None
+    * @return	integer - 0 if program executed successfully, else nonzero will be returned
+	* @pre	Precondition: None
+	* @post	Postcondition: 
+	* Method Output: 
+	*
+*****************************************************************************************************************************/
+int main()
+{
+	
+	///return 0 if program executed successfully because main function is of type integer
+	return 0;
+}
---
+++ readme.txt
@@ -0,0 +1,17 @@
+There are 5 files:
+1. Output Screenshots: VS Code and Command Prompt
+2. Github repository & version history screenshots
+3. The Source Code(MorseCode.cpp file)
+4. The Header File(Node.h)
+5. Doxygen generated html file - check index.html in that folder
+
+This program has been tested and can be run in Visual Studio Code & Command Prompt on Windows 10 with Mingw C++ Compiler
+
+Github link for this program:
+https://github.com/faizasiddiqui33/Assignment5.git
+
+MorseCode.cpp is a C++ program that is a user-friendly and it does the following:
+	- Prompts the user to enter a message to encode
+	- It then encodes that message using morse code and displays an encoded message in the form of dots and dashes
+	- It creates a tree for storing all the alphabets, the tree has a depth of 4. 
+	- It then decodes the morse code and returns the original message entered by the user.
