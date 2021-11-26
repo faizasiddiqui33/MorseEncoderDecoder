560904ab78dbd06cae820424790e450322033211
Faiza Siddiqui <faizasiddiqui@uregina.ca>
2021-11-26 03:16:25 +0300

    Adding authorship comments in header file

--- Tree.h
+++ Tree.h
@@ -1,4 +1,21 @@
+/*****************************************************************************************************************************
+	* @file		Tree.h
+    * @brief	Module Name is Tree Header File
+    * @author	Faiza Fatma Siddiqui, StudentID: 200473896
+    * @date		25-11-2021 (Created & Modified)
+    * @details 	This header file has a class which includes:
+	* 	- A default & parametrized constructor to initialize values 
+	*	- User-defined functions to create a binary tree, find a letter from the binary tree, 
+	*	  find a morse code from the letter in the binary tree, encode a message received from the main function, 
+	*	  decode a message from morse code
+    * Purpose:	To perform all the tree functions: Creating, Inserting nodes, Finding nodes, Encoding, Decoding
+	* Method Output: Tree is created, message is encoded & decoded in morse code & returned to the main function
+	* @pre		Use only alphabets to encode, not numbers or symbols
+    * @bug		No known bugs
+	* @warning	Improper use can crash the program while taking input from the user
+*****************************************************************************************************************************/
 ///@file tree.h
+
 #ifndef TREE_H
 #define TREE_H
 
